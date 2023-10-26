9375af9a463e44c611f8044c3f2859415ed5436d
tolaoni11 <tolaoni11@yahoo.com>
2023-10-26 10:54:01 -0500

    Objects and Array of Objects

--- src/components/Example5.jsx
+++ src/components/Example5.jsx
@@ -9,15 +9,48 @@ export default function Example5() {
   //array always start with zero
   //first is zero
   const hobbies = ["movies", "music", "sports"];
+
   //object
+  const address = {
+    street: "50 Main st",
+    city: "Boston",
+    state: "MA",
+  };
 
-  //map function
+  //Array of Objects
+  {
+    const todos = [
+      {
+        id: 1,
+        text: "Take out trash",
+        isCompleted: true,
+      },
+      {
+        id: 2,
+        text: "Meeting the boss",
+        isCompleted: true,
+      },
+      {
+        id: 3,
+        text: "Dentist appt",
+        isCompleted: false,
+      },
+    ];
 
-  return (
-    <>
-      <div>{name}</div>
-      <div>{age}</div>
-      <div>{hobbies[3]}</div>
-    </>
-  );
+    //map function
+
+    return (
+      <>
+        <div>{name}</div>
+        <div>{age}</div>
+        <div>{hobbies[0]}</div>
+        <div>{address.city}</div>
+        <div>{address["state"]}</div>
+        <div>{todos[0]["text"]}</div>
+        <div>{todos[0].text}</div>
+        <div>{todos[1]["id"]}</div>
+        <div>{todos[2]["isCompleted"]}</div>
+      </>
+    );
+  }
 }
